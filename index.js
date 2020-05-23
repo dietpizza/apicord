037d0600f492fd8d44a7c2e7ec8e8aaed423dd7a
Rohan Biswas <brainhacker550@gmail.com>
2020-05-23 16:45:34 +0530

    refactoring

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
-// Dependencies
 const express = require('express');
 const cors = require('cors');
 
-// Relative dependencies
-const socketio = require('./sockets');
+const sockets = require('./sockets');
 const MongoInterface = require('./db');
 const MongoDB = require('mongodb').MongoClient;
 const { MONGO_URI } = require('./config');
 
-// Global Variables
 const PORT = process.env.PORT || 3000;
 
-// MongoDB config
-var db = undefined;
 const client = new MongoDB(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   poolSize: 10,
 });
-
-// Express instance
 const app = express();
 
-// Adding middlewares to express instance
 app.use(cors());
 app.use(express.json());
-// Connecting to MongoDB
+
+const cleanup = function () {
+  client.close().then(() => {
+    console.log('\nConnection to MongoDB closed.');
+    process.exit(0);
+  });
+};
+
 client.connect((err) => {
   if (err) {
     console.log('Error connecting to MongoDB.');
     process.exit(0);
   } else {
-    db = new MongoInterface(client);
-    // Cleanup function
-    const cleanup = function () {
-      client.close().then(() => {
-        console.log('\nConnection to MongoDB closed.');
-        process.exit(0);
-      });
-    };
-    // Starting express server
+    var db = new MongoInterface(client);
     const server = app.listen(PORT, () => {
       console.log('Server running at port: ' + PORT);
       process.on('SIGTERM', cleanup);
@@ -47,7 +38,6 @@ client.connect((err) => {
     });
     const routes = require('./routes')(db);
     app.use('/', routes);
-    // Socket.io
-    socketio(server, db);
+    sockets(server, db);
   }
 });
