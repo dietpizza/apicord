8b7104004fb1ecb60e90c16afcb0a902c6f91470
Rohan Biswas <brainhacker550@gmail.com>
2020-05-23 16:50:39 +0530

    fixed config bug

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
 module.exports = {
   JWT_KEY: '0f348563672d11275facc16e2d4e35ced339217e5ee1983e69f0dc3e12bd47d1',
-  MONGO_USER: 'rohan',
-  MONGO_PASS: 'kepsake550',
-  MONGO_URI:
-    'mongodb+srv://' +
-    this.MONGO_USER +
-    ':' +
-    this.MONGO_PASS +
-    '@cluster0-mvzld.azure.mongodb.net/',
+  MONGO_URI: 'mongodb+srv://rohan:kepsake550@cluster0-mvzld.azure.mongodb.net/',
 };
