df871d789501ddd7bbb88ad28d58933ce3ba47da
Rohan Biswas <brainhacker550@gmail.com>
2020-06-21 22:13:22 +0530

    okey

--- routes.js
+++ routes.js
@@ -1,26 +1,26 @@
 function routes(db) {
-  const sha256 = require("sha256");
-  const router = require("express").Router();
-  const jwt = require("jsonwebtoken");
-  const { JWT_KEY } = require("./config");
+  const sha256 = require('sha256');
+  const router = require('express').Router();
+  const jwt = require('jsonwebtoken');
+  const { JWT_KEY } = require('./config');
 
   function authorize(req, res, next) {
     if (req.body.token != undefined) {
       jwt.verify(req.body.token, JWT_KEY, (err, decoded) => {
         if (err) {
-          res.status(401).json({ error: "Not authorized!" });
+          res.status(401).json({ error: 'Not authorized!' });
           return;
         }
         res.locals.user = decoded.user;
         return next();
       });
     } else {
-      res.status(401).json({ error: "Not authorized!" });
+      res.status(401).json({ error: 'Not authorized!' });
       return;
     }
   }
 
-  router.post("/api/login", (req, res) => {
+  router.post('/api/login', (req, res) => {
     db.authenticate(req.body.username, sha256(req.body.passwd), (data) => {
       var response = {
         token: undefined,
@@ -29,17 +29,17 @@ function routes(db) {
         response.token = jwt.sign({ user: data.user }, JWT_KEY);
         res.status(200).json(response);
       } else {
-        res.status(401).json({ error: "Username or password incorrect" });
+        res.status(401).json({ error: 'Username or password incorrect' });
       }
     });
   });
 
-  router.post("/api/auth", authorize, (req, res) => {
+  router.post('/api/auth', authorize, (req, res) => {
     var response = { token: jwt.sign({ user: res.locals.user }, JWT_KEY) };
     res.json(response);
   });
 
-  router.post("/api/register", (req, res) => {
+  router.post('/api/register', (req, res) => {
     var user = {
       fname: req.body.fname,
       lname: req.body.lname,
@@ -50,16 +50,16 @@ function routes(db) {
     };
     db.addUser(user, (response) => {
       if (response.status == 200) {
-        res.status(200).json({ message: "User Registered!" });
+        res.status(200).json({ message: 'User Registered!' });
       } else {
         res
           .status(response.status)
-          .json({ error: "Username or email already registered!" });
+          .json({ error: 'Username or email already registered!' });
       }
     });
   });
 
-  router.post("/api/users", authorize, (req, res) => {
+  router.post('/api/users', authorize, (req, res) => {
     db.getUsers((response) => {
       user = res.locals.user;
       response.users.forEach((user) => {
@@ -73,25 +73,25 @@ function routes(db) {
     });
   });
 
-  router.post("/api/messages", authorize, (req, res) => {
+  router.post('/api/messages', authorize, (req, res) => {
     user = res.locals.user;
     db.getMessages(user.id, req.body.target, (data) => {
       res.status(data.status).json(data.messages);
     });
   });
 
-  router.get("/api", (req, res) => {
+  router.get('/api', (req, res) => {
     res.status(200).json({
-      message: "Server is running",
+      message: 'Server is running',
     });
   });
 
-  router.post("*", (req, res) => {
-    res.status(404).json({ error: "Page not found!" });
+  router.post('*', (req, res) => {
+    res.status(404).json({ error: 'Page not found!' });
   });
 
-  router.get("*", (req, res) => {
-    res.status(404).json({ error: "Page not found!" });
+  router.get('*', (req, res) => {
+    res.status(404).json({ error: 'Page not found!' });
   });
   return router;
 }
